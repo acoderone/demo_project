6e1044b7a3bd28008952a27ec5373dc82d572099
Acoderone <avi5200786@gmail.com>
2024-10-19 15:00:16 +0530

    routed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Register from './components/register';
+import Login from './components/login';
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+<Routes>
+<Route path='/register' element={<Register />}/>
+<Route path='/login' element={<Login />}/>
+
+</Routes>
+    </Router>
   )
 }
 
